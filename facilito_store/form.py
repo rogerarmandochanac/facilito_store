58aca010690bf40fcc0cf877c8e391e91ba65ba6
rogerarmandochanac <armandoroger007@gmail.com>
2025-02-23 17:32:14 +0000

    Registro de usuarios y validaciones

--- facilito_store/form.py
+++ facilito_store/form.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -6,3 +7,22 @@ class RegisterForm(forms.Form):
     }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class':'form-control'
+    }))
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("El nombre usuario ya existe")
+    
+    def clean(self):
+        password1 = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+        if password1 != password2:
+            self.add_error("password2", "Las contraseñas no coinciden")
+    def save(self):
+        return User.objects.create_user(
+            username=self.cleaned_data.get("username"),
+            email = self.cleaned_data.get("email"),
+            password= self.cleaned_data.get("password")
+        )
--- facilito_store/views.py
+++ facilito_store/views.py
@@ -9,6 +9,10 @@ from django.contrib import messages
 
 from .form import RegisterForm
 
+from django.contrib.auth.models import User
+
+from django import forms
+
 def index_view(request):
     return render(request, 'index.html', {})
 
@@ -29,5 +33,9 @@ def logout_view(request):
     return redirect("login")
 
 def register_view(request):
-    form = RegisterForm()
+    form = RegisterForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        if form.save():
+            messages.success(request, "Usuario creado exitosamente")
+            return redirect("index")
     return render(request, "users/register.html", {'form':form})
