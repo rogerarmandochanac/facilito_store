c54fbdce4503d908127d5dd707c4ede36f8ba543
rogerarmandochanac <armandoroger007@gmail.com>
2025-04-13 09:51:43 -0600

    first template, jinja for if staticfiles settings

--- facilito_store/settings.py
+++ facilito_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'facilito_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
--- facilito_store/views.py
+++ facilito_store/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hola desde el Archivo views.py')
+    return render(request, 'index.html', {
+        "message":"Listado de productos",
+        "products":[
+            {'title': "Playera", "Precio":"$5", "stock":True},
+            {'title': "Camisa", "Precio":"$7", "stock":True},
+            {'title': "Mochila", "Precio":"$20", "stock":False},
+        ]
+        })
