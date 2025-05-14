2aa35489eb453ed64a32bf446cfffa9cf8312e61
rogerarmandochanac <armandoroger007@gmail.com>
2025-05-14 11:29:06 -0600

    Productos/Buscador de productos

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductDetailView
+from .views import ProductDetailView, ProductSearchListView
 
 urlpatterns = [
+    path("search", ProductSearchListView.as_view(), name='search'),
     path("<slug:slug>", ProductDetailView.as_view(), name='product'),
+    
 ]
--- products/views.py
+++ products/views.py
@@ -15,3 +15,18 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product/product.html'
+
+class ProductSearchListView(ListView):
+    template_name = 'product/search.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(title__icontains=self.query())
+    
+    def query(self):
+        return self.request.GET.get('q')
+    
+    def get_context_data(self, **k):
+        context = super().get_context_data(**k)
+        context['query'] = self.query()
+        context['count'] = context['object_list'].count()
+        return context
